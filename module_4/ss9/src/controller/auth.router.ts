9f603a5c95ab768d0dbd262cf324427f08981661
haitan2408 <haitan28102408@gmail.com>
2023-05-16 20:59:35 +0700

    haitt up ss9

---
+++ module_4/ss9/src/controller/auth.router.ts
@@ -0,0 +1,42 @@
+
+import express, {Router} from "express";
+import {UserModel} from "../model/use.model";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+const authRouter = Router();
+
+authRouter.get('/login', (req, res) => {
+    res.render("login");
+});
+
+authRouter.post('/login', async (req, res, next) => {
+    try {
+        const user = await UserModel.findOne({ username: req.body.username });
+        if (user) {
+            const comparePass = await bcrypt.compare(req.body.password, user.password);
+            if (!comparePass) {
+                return Promise.reject({
+                    code: 404,
+                    message: "PASSWORD_NOT_VALID",
+                });
+            }
+            let payload = {
+                user_id: user["id"],
+                username: user["username"],
+                role: user["role"]
+            }
+
+            const token = jwt.sign(payload, '123456789', {
+                expiresIn: 36000,
+            });
+            return res.json({token: token, code: 200})
+        } else {
+            return res.json({ err: 'Email has been used' });
+        }
+    } catch (err) {
+        return res.json({ err: err })
+    }
+});
+
+export default authRouter;
+
